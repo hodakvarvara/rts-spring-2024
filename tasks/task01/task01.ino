7c2e344e93e649802da69bb40f671da0f98d9873
hodakvarvara <122392471+hodakvarvara@users.noreply.github.com>
2024-04-07 19:01:26 +0400

    Update task01.ino

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -10,14 +10,20 @@ const int ledPin10 = 10;
 const int interval3 = 10000;  
 const int interval5 = 1000;  
 const int interval6 = 500;  
-const int interval9 = 9;  
-const int interval10 = 10;
+const int interval9 = 100;  
+const int interval10 = 50;
 
 unsigned long previousMillis3 = 0; 
 unsigned long previousMillis5 = 0; 
 unsigned long previousMillis6 = 0; 
 unsigned long previousMillis9 = 0; 
 unsigned long previousMillis10 = 0; 
+
+int ledStatePin3 = LOW;
+int ledStatePin5 = LOW;
+int ledStatePin6 = LOW;
+int ledStatePin9 = LOW;
+int ledStatePin10 = LOW;
 //int ledState = LOW;  // ledState used to set the LED
 //unsigned long previousMillis = 0;  // will store last time LED was updated
 //const long interval = 10;  // interval at which to blink (milliseconds)
@@ -30,25 +36,25 @@ void setup() {
   pinMode(ledPin10, OUTPUT);
 }
 
-int func(unsigned long previousTime, long interval, int ledState, int ledPin)
-{
-  unsigned long currentMillis = millis() * 1000;
-  if (currentMillis - previousTime >= interval) {
-    previousTime = currentMillis;
+void toggleLED(unsigned long currentMillis, unsigned long& previousMillis, unsigned long interval, int pin, int& ledState) {
+  if (currentMillis * 1000 - previousMillis >= interval) {
+    previousMillis = currentMillis * 1000;
     if (ledState == LOW) {
       ledState = HIGH;
     } else {
       ledState = LOW;
     }
-    digitalWrite(ledPin, ledState);
+    digitalWrite(pin, ledState);
   }
-  return previousTime;
 }
 
 void loop() {
-  previousMillis3 = func(previousMillis3, interval3, LOW, ledPin3);
-  previousMillis5 = func(previousMillis5, interval5, LOW, ledPin5);
-  previousMillis6 =func(previousMillis6, interval6, LOW, ledPin6);
-  previousMillis9 = func(previousMillis9, interval9, LOW, ledPin9);
-  previousMillis10 = func(previousMillis10, interval10, LOW, ledPin10);
+   unsigned long currentMillis = millis();
+
+  toggleLED(currentMillis, previousMillis3, interval3, ledPin3, ledStatePin3);
+  toggleLED(currentMillis, previousMillis5, interval5, ledPin5, ledStatePin5);
+  toggleLED(currentMillis, previousMillis6, interval6, ledPin6, ledStatePin6);
+  toggleLED(currentMillis, previousMillis9, interval9, ledPin9, ledStatePin9);
+  toggleLED(currentMillis, previousMillis10, interval10, ledPin10, ledStatePin10);
+
 }
